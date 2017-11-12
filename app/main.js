07bc8ca9d951f8e69df259e046986b1149913181
Exxion <exxion191@gmail.com>
2017-11-11 23:20:36 -0500

    Add client HTML and test code

---
+++ app/main.js
@@ -0,0 +1,26 @@
+//Temporary. Just a static animation to show that the viewport works.
+
+var c = document.getElementById("mapwindow");
+var ctx = c.getContext("2d");
+var counter = 0;
+var space = new Image();
+space.src = '/space.png';
+var clown = new Image();
+clown.src = '/clown.gif';
+
+function animate(){
+	c.height = c.clientHeight;
+	c.width = c.height;
+	ctx.save();
+	ctx.drawImage(space, 0, 0, c.width, c.height);
+	ctx.translate((c.width / 2 + ++counter) % (c.width + 200) - 100, c.height / 2);
+	ctx.rotate(counter / 100);
+	ctx.drawImage(clown, -50, -50, 100, 100);
+	ctx.restore();
+
+	window.requestAnimationFrame(animate);
+}
+
+window.onload = function(){
+	animate();
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
     input: 'app/main.js',
     output: {
